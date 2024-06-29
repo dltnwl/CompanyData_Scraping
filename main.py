a4088cb39dd1370da4740c2b7628c5a45c59518b
LEE <gagashi@naver.com>
2024-06-29 21:46:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,36 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+# 스크래핑 수행하기 
+import subprocess
+from Scrap.date_time import get_first_and_last_day_of_previous_month
 
+# Scrap 폴더 안의 스크립트 실행 목록
+scripts = [
+    '001_NewTech.py',
+    '002_InnoBiz.py',
+    '003_Mainbiz.py',
+    '004_CompanyLab.py'
+]
+
+# 각 스크립트를 subprocess로 실행
+for script in scripts:
+    script_path = f"Scrap/{script}"  # 스크립트 경로
+    print(f"Running {script_path}...")  # 실행 중인 스크립트 출력
+    result = subprocess.run(['python', script_path], capture_output=True, text=True)
+
+    # 실행 결과 출력
+    if result.returncode == 0:
+        print("Output:")
+        print(result.stdout)  # 스크립트 실행 결과 출력
+    else:
+        print("Error:")
+        print(result.stderr)  # 에러 메시지 출력
+
+print("All scripts executed.")
+
+
+############메일보내기#################
 # SMTP 인스턴스 생성
 smtp = smtplib.SMTP('smtp.gmail.com', 587)
 
@@ -23,22 +52,30 @@ body = '본문: Hello, this is a test email with attachment.'
 msg.attach(MIMEText(body, 'plain'))
 
 # 첨부 파일 경로
-filename = '001_NewTech_202405.csv'
-filepath = filename  # 파일 경로 설정
 
-# MIMEBase 객체 생성
-attachment = open(filepath, "rb")  # 파일을 바이너리 형식으로 읽기
+filenames = [f'001_NewTech_{formatted_date}.csv', 
+             f'002_Innobiz_{formatted_date}.csv', 
+             f'003_Mainbiz_{formatted_date}.csv', 
+             f'004_Company_lab_{formatted_date}.csv'
+            ]
+folder_path = 'Scrap/'  # 파일이 위치한 폴더 경로
 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())  # 파일 내용을 MIMEBase 객체에 추가
-encoders.encode_base64(part)  # Base64 인코딩 수행
+# 파일 첨부 과정
+for filename in filenames:
+    filepath = f"{folder_path}{filename}"
+    attachment = open(filepath, "rb")  # 파일을 바이너리 형식으로 읽기
 
-part.add_header(
-    'Content-Disposition',
-    f'attachment; filename= {filename}',
-)
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload(attachment.read())
+    encoders.encode_base64(part)  # Base64 인코딩 수행
 
-msg.attach(part)  # 완성된 파트(첨부 파일)를 메시지 객체에 추가
+    part.add_header(
+        'Content-Disposition',
+        f'attachment; filename="{filename}"'
+    )
+    msg.attach(part)  # 메시지 객체에 파일 첨부
+    attachment.close()  # 파일 닫기
+
 
 # 메일 보내기
 smtp.sendmail('dltnwl93@gmail.com', 'dltnwl9322@gmail.com', msg.as_string())
