93ef4324a07cb7e5b4423e82541b36483ecdbe9d
LEE <gagashi@naver.com>
2024-06-30 12:32:51 +0900

    Update 004_CompanyLab.py

--- Scrap/004_CompanyLab.py
+++ Scrap/004_CompanyLab.py
@@ -28,16 +28,18 @@ session = requests.Session()
 # POST 요청 보내기
 response = session.post(download_url, data=payload)
 
+Directory='Data'
+
 # 응답 확인 및 파일 저장
 if response.status_code == 200:
-    with open('Data/companylab_download.xlsx', 'wb') as file:
+    with open('{Directory}/companylab_download.xlsx', 'wb') as file:
         file.write(response.content)
 
-    df = pd.read_excel('Data/companylab_download.xlsx')
+    df = pd.read_excel('{Directory}/companylab_download.xlsx')
     
     df.insert(0, 'GB', '004')
     #df2=df.drop(columns=['Unnamed: 0'])
-    df.to_csv(f'Data/004_Company_lab_{formatted_date}.csv', index=False, sep=',')
+    df.to_csv(f'{Directory}/004_Company_lab_{formatted_date}.csv', index=False, sep=',')
     
     print("엑셀 파일 다운로드 완료")
 else:
