4634d2841371356dbcb2040f46ec66576c4a6496
LEE <gagashi@naver.com>
2024-06-30 19:05:11 +0900

    Update 015_Fire.py

--- Scrap/015_Fire.py
+++ Scrap/015_Fire.py
@@ -15,7 +15,6 @@ formatted_date = first_day_of_previous_month.strftime('%Y%m')
 year=first_day_of_previous_month.year
 month=first_day_of_previous_month.month
 
-Directory='Data'
 
 def fetch_data(url):
     response = requests.get(url, verify=False)
@@ -36,8 +35,12 @@ def fetch_data(url):
     df = pd.DataFrame(data, columns=headers)
     return df
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+}
+response = requests.get('https://ftis.ekffa.or.kr/fe/construct/NR_listRanking.do?currentPage=1&year=2023&searchType=1000', headers=headers, verify=False, timeout=5)
 
-response = requests.get(f'https://ftis.ekffa.or.kr/fe/construct/NR_listRanking.do?currentPage=1&year=2023&searchType=1000', verify=False) # 나중에 수정해주기
+
 soup = BeautifulSoup(response.text, 'html.parser')
 count_div = soup.find('p', class_='board-count')
 number_of_companies = re.findall(r'[^\uAC00-\uD7A3]+',count_div.span.text)
@@ -50,11 +53,11 @@ end_page=math.ceil(int(number)/10)
 
 result_df_f=[]
 for i in range(1,end_page+1):
-    url=f'https://ftis.ekffa.or.kr/fe/construct/NR_listRanking.do?currentPage={i}&year=2023&searchType=1000' # 수정 {year}
+    url=f'https://ftis.ekffa.or.kr/fe/construct/NR_listRanking.do?currentPage={i}&year=2023&searchType=1000'
     result_df = fetch_data(url)
     result_df_f.append(result_df)
 
 df=pd.concat(result_df_f)
 df.insert(0, 'GB', '015')
-df.to_csv(f'{Directory}/015_Fire_{formatted_date}.csv', index=False, encoding='utf-8-sig')
+df.to_csv(f'015_Fire_{formatted_date}.csv', index=False, encoding='utf-8-sig')
 print(df)
