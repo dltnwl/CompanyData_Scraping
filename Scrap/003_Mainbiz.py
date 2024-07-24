0ed6f7a13c3182e29ed517a28cade66e349b2f37
LEE <gagashi@naver.com>
2024-07-24 21:41:23 +0900

    Update 003_Mainbiz.py

--- Scrap/003_Mainbiz.py
+++ Scrap/003_Mainbiz.py
@@ -64,7 +64,7 @@ if response.status_code == 200:
                 
                     df.insert(0, 'GB', '003')
                     df2=df.drop(columns=['번호'])
-                    df2.to_csv(f'{Directory}/003_Mainbiz_{formatted_date}.csv', index=False, sep=',')
+                    df2.to_csv(f'{Directory}/003_Mainbiz_{formatted_date}.csv', index=False, sep=',', encoding='utf-8-sig')
                 else:
                     print("첨부파일 다운로드 실패:", file_response.status_code)
             else:
